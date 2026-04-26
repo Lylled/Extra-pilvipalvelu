9cc003f0eef243a36c266d30e1a75329dd3f2012
Lylled <liinamaalyydi@gmail.com>
2026-04-26 19:49:56 +0300

    Prepare app for Render deployment

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const http = require("http");
 
+const PORT = process.env.PORT || 3000;
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Hello World!");
 });
 
-server.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
